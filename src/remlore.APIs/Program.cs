e56d7135aa6a227ea0e039fcf9f6ef92ccd6f4ed
Trung Nguyen <averaysia@gmail.com>
2025-08-04 02:08:53 +0000

    Revert "chore: Deleted remlore.APIs"

---
+++ src/remlore.APIs/Program.cs
@@ -0,0 +1,33 @@
+using Remlore.Application;
+using Remlore.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Remlore API V1");
+        options.OAuthClientId("remlore_swagger_api");
+        options.OAuthClientSecret("super_secret");
+    });
+}
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
